53223bd5909132c8e85be7f3ea0d273e829fd1d1
wcdaren <wcdaren@gmail.com>
2019-03-22 14:48:43 +0800

    配置 less 和 babel-plugin-import

---
+++ src/components/Test.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react'
+import { Button } from 'antd'
+// import 'antd/dist/antd.css'
+import './test.less'
+
+
+class Vote extends React.Component {
+    render() {
+        return <div>
+            hello world
+            {/* <Button>删除</Button> */}
+            <div className="test">sdf</div>
+            <Button>test</Button>
+            </div>
+    }
+}
+
+export default Vote
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { PureComponent } from 'react'
+import ReactDOM, { render } from 'react-dom'
+import PropTypes from 'prop-types'
+import Test from './components/Test'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+render(<Test></Test>, document.getElementById('root'))
